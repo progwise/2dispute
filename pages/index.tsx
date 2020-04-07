b68d5cab1466321a0af0f514e9f89002b20ad585
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-07 16:16:30 +0200

    Add ESLint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import Head from 'next/head';
 
-const Home = () => (
+const Home = (): JSX.Element => (
   <div className="container">
     <Head>
       <title>Create Next App</title>
