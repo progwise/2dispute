7f05bae1a697a00a0cc54f5d95fb0e95dda9428b
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-08 13:19:18 +0200

    Remove unused CSS from production bundle

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,20 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: [
+      './components/**/*.{js,jsx,ts,tsx}',
+      './pages/**/*.{js,jsx,ts,tsx}',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
 module.exports = {
-  plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 };
