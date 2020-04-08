f50f71ebef56a447f12f62212311c790c213e700
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-08 16:17:46 +0200

    Display tweet after the user refer an url

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 interface InputProps {
   label: string;
   placeholder: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ label, placeholder }: InputProps): JSX.Element => (
+const Input = ({ label, placeholder, onChange }: InputProps): JSX.Element => (
   <label className="w-full flex flex-col text-center py-2">
     <span className="text-blue-600">{label}</span>
     <input
       className="border-2 rounded text-center text-lg"
       placeholder={placeholder}
+      onChange={onChange}
     />
   </label>
 );
--- pages/dispute/create.tsx
+++ pages/dispute/create.tsx
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 
-const CreateDispute = (): JSX.Element => (
-  <>
-    <h1 className="text-xl text-center py-4">Create a Dispute</h1>
-    <div>
-      <Input label="Das Thema" placeholder="Neues Thema" />
-      <Input label="Twitter Link" placeholder="https://twitter.com/..." />
-      <div className="grid grid-cols-4">
-        <div className="col-span-2 flex flex-col items-center py-4">
-          <img
-            src="https://www.progwise.net/static/c7930b51fec4d6e53d183f2cefd721e7/9af95/michael-vogt.jpg"
-            className="rounded-full w-32"
-          />
-          <span>Du</span>
+const CreateDispute = (): JSX.Element => {
+  const [tweetId, setTweetId] = useState<string>(undefined);
+
+  const handleTweetChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    const twitterLink = event.target.value;
+
+    const twitterRegex = /twitter\.com\/(?:\w+)\/status(?:es)?\/(\d+)/i;
+    const [, foundTweetId] = twitterLink.match(twitterRegex) ?? [
+      undefined,
+      undefined,
+    ];
+    setTweetId(foundTweetId);
+  };
+
+  return (
+    <>
+      <h1 className="text-xl text-center py-4">Create a Dispute</h1>
+      <div>
+        <Input label="Das Thema" placeholder="Neues Thema" />
+        <Input
+          label="Twitter Link"
+          placeholder="https://twitter.com/..."
+          onChange={handleTweetChange}
+        />
+        {tweetId && (
+          <div className="px-4 mx-auto">
+            <TwitterTweetEmbed
+              tweetId={tweetId}
+              key={tweetId}
+              placeholder="Lade Tweet..."
+              className="mx-auto"
+              options={{
+                lang: 'de',
+                align: 'center',
+              }}
+            />
+          </div>
+        )}
+        <div className="grid grid-cols-4">
+          <div className="col-span-2 flex flex-col items-center py-4">
+            <img
+              src="https://www.progwise.net/static/c7930b51fec4d6e53d183f2cefd721e7/9af95/michael-vogt.jpg"
+              className="rounded-full w-32"
+            />
+            <span>Du</span>
+          </div>
+          <div className="col-span-2 flex flex-col items-center py-4">
+            <img
+              src="https://www.progwise.net/static/95ed2645d74a2c245115ee4b1b2dc864/9af95/pascal-helbig.jpg"
+              className="rounded-full w-32"
+            />
+            <span>???</span>
+          </div>
+          <textarea
+            className="col-start-1 col-span-3 border-2"
+            placeholder="Deine Position"
+          ></textarea>
         </div>
-        <div className="col-span-2 flex flex-col items-center py-4">
-          <img
-            src="https://www.progwise.net/static/95ed2645d74a2c245115ee4b1b2dc864/9af95/pascal-helbig.jpg"
-            className="rounded-full w-32"
-          />
-          <span>???</span>
-        </div>
-        <textarea
-          className="col-start-1 col-span-3 border-2"
-          placeholder="Deine Position"
-        ></textarea>
       </div>
-    </div>
-    <div className="py-4">
-      <Button>Veröffentlichen</Button>
-    </div>
-  </>
-);
+      <div className="py-4">
+        <Button>Veröffentlichen</Button>
+      </div>
+    </>
+  );
+};
 
 export default CreateDispute;
