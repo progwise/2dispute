4138bb9b1a412c554e4abdb84a86acc30dfbd746
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-14 12:43:48 +0200

    Add Formik for form handling

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 interface ButtonProps {
   children: string;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const Button = ({ children }: ButtonProps): JSX.Element => (
-  <button className="bg-blue-600 hover:bg-blue-700 p-2 rounded-md text-white transition duration-100 shadow-md">
+const Button = ({ children, type }: ButtonProps): JSX.Element => (
+  <button
+    className="bg-blue-600 hover:bg-blue-700 p-2 rounded-md text-white transition duration-100 shadow-md"
+    type={type}
+  >
     {children}
   </button>
 );
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 
 interface InputProps {
   label: string;
+  name?: string;
   placeholder: string;
+  value?: string | number;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ label, placeholder, onChange }: InputProps): JSX.Element => (
+const Input = ({
+  label,
+  placeholder,
+  onChange,
+  name,
+  value,
+}: InputProps): JSX.Element => (
   <label className="w-full flex flex-col text-center py-2">
     <span className="text-blue-600">{label}</span>
     <input
       className="border-2 rounded text-center text-lg"
+      name={name}
       placeholder={placeholder}
       onChange={onChange}
+      value={value}
     />
   </label>
 );
