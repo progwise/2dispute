e9fc651c04dd7d07967749c6161086420e5aecbf
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-15 15:06:44 +0200

    Move getUserById function into own file

---
+++ graphql/User/getUserById.ts
@@ -0,0 +1,16 @@
+import { User } from '../generated/graphql';
+import Auth0ManagementClient from './auth0ManagementClient';
+
+const getUserById = async (userId: string): Promise<User> => {
+  const user = await Auth0ManagementClient.getUser({ id: userId });
+  if (user.nickname === undefined) {
+    throw new Error('Username not found');
+  }
+
+  return {
+    id: userId,
+    name: user.nickname,
+  };
+};
+
+export default getUserById;
--- graphql/User/index.ts
+++ graphql/User/index.ts
@@ -1,14 +1,1 @@
-import { User } from '../generated/graphql';
-import Auth0ManagementClient from './auth0ManagementClient';
-
-export const getUserById = async (userId: string): Promise<User> => {
-  const user = await Auth0ManagementClient.getUser({ id: userId });
-  if (user.nickname === undefined) {
-    throw new Error('Username not found');
-  }
-
-  return {
-    id: userId,
-    name: user.nickname,
-  };
-};
+export { default as getUserById } from './getUserById';
