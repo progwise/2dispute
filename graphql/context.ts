90d1fba9b6508ee66d7a7aaaffa2c40ad2faaf0c
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-17 17:24:43 +0200

    Add dummy entries for testing

--- graphql/context.ts
+++ graphql/context.ts
@@ -40,7 +40,42 @@ export interface SubjectStoreItem {
 }
 export type SubjectStore = SubjectStoreItem[];
 
-let subjectStore: SubjectStore = [];
+let subjectStore: SubjectStore = [
+  {
+    id: '123',
+    subject: 'Test Subject',
+    tweetId: '1250049436799111168',
+    userId: 'auth0|5e8ee039658e660c13004a03',
+    firstMessage: 'Meiner Meinung nach...',
+    disputes: [
+      {
+        id: '2',
+        partnerIdA: 'auth0|5e8ee039658e660c13004a03',
+        partnerIdB: 'auth0|5e8f367c9374050c0b984f1e',
+        messages: [
+          {
+            id: '3',
+            authorId: 'auth0|5e8ee039658e660c13004a03',
+            text: 'Meiner Meinung nach...',
+          },
+          {
+            id: '4',
+            authorId: 'auth0|5e8f367c9374050c0b984f1e',
+            text: 'Ich habe da eine andere Meinung',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: '1234',
+    subject: 'Test 2',
+    tweetId: null,
+    userId: 'auth0|5e8f367c9374050c0b984f1e',
+    firstMessage: 'Test2...',
+    disputes: [],
+  },
+];
 
 const context = async ({ req }: { req: NextApiRequest }): Promise<Context> => {
   const session = await auth0(req).getSession(req);
