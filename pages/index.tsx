7e6ce569938939fefd191f68108b900abb0c582f
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-24 15:26:06 +0200

    Startpage: Display hint if no dispute or subject was found

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,36 +24,44 @@ const Home = (): JSX.Element => {
     <>
       <div className="py-4">
         <h1>Aktuelle Dispute:</h1>
-        <ul className="list-disc pl-8">
-          {data.latestActiveDisputes.edges.map(({ node: dispute }) => (
-            <Link
-              key={dispute.id}
-              href="/dispute/[disputeId]"
-              as={`/dispute/${dispute.id}`}
-            >
-              <li>
-                {dispute.subject.subject} (Dispute zwischen{' '}
-                {dispute.partnerA.name} und {dispute.partnerB.name})
-              </li>
-            </Link>
-          ))}
-        </ul>
+        {data.latestActiveDisputes.edges.length > 0 ? (
+          <ul className="list-disc pl-8">
+            {data.latestActiveDisputes.edges.map(({ node: dispute }) => (
+              <Link
+                key={dispute.id}
+                href="/dispute/[disputeId]"
+                as={`/dispute/${dispute.id}`}
+              >
+                <li>
+                  {dispute.subject.subject} (Dispute zwischen{' '}
+                  {dispute.partnerA.name} und {dispute.partnerB.name})
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <span>Keine Dispute</span>
+        )}
       </div>
       <div className="py-4">
         <h1>Unbeantwortete Themen:</h1>
-        <ul className="list-disc pl-8">
-          {data.unansweredSubjects.edges.map(({ node: subject }) => (
-            <Link
-              key={subject.id}
-              href="/subjects/[subjectId]"
-              as={`/subjects/${subject.id}`}
-            >
-              <li>
-                {subject.subject} von {subject.author.name}
-              </li>
-            </Link>
-          ))}
-        </ul>
+        {data.unansweredSubjects.edges.length > 0 ? (
+          <ul className="list-disc pl-8">
+            {data.unansweredSubjects.edges.map(({ node: subject }) => (
+              <Link
+                key={subject.id}
+                href="/subjects/[subjectId]"
+                as={`/subjects/${subject.id}`}
+              >
+                <li>
+                  {subject.subject} von {subject.author.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <span>Keine unbeantwortete Themen</span>
+        )}
       </div>
     </>
   );
