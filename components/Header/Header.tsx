995cc629539cc31d79c916d5597856b3a5095d9e
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-27 10:27:33 +0200

    Add auth link to header
    Display sign in link when the user is unauthenticated and a sign out
    when the user is authenticated.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { useHeaderMeQuery } from '../../graphql/generated/graphql';
 import withApollo from '../../utils/withApollo';
+import Link from '../Link/Link';
 
 const Header = (): JSX.Element => {
   const { loading, data } = useHeaderMeQuery();
+  const router = useRouter();
 
   let userName: string;
+  let authLink: JSX.Element | undefined = undefined;
 
   if (data?.me) {
     userName = data.me.name;
+    authLink = <Link href="/api/logout">abmelden</Link>;
   } else if (loading) {
     userName = 'loading...';
   } else {
     userName = 'unangemeldet';
+    authLink = (
+      <Link href={`/api/login?redirectTo=${router.asPath}`}>anmelden</Link>
+    );
   }
 
   return (
     <header className="w-full py-4 flex flex-col items-center">
       <span className="text-2xl">2 Dispute</span>
-      <span>Benutzer: {userName}</span>
+      <span>
+        Benutzer: {userName} {authLink && <>({authLink})</>}
+      </span>
     </header>
   );
 };
