9c3af32bd01cdb9d64ab31fedba08e3c37562b6c
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-28 11:57:57 +0200

    Start vertical line in chat lower

--- components/Chat/components/ChatContainer.tsx
+++ components/Chat/components/ChatContainer.tsx
@@ -7,7 +7,7 @@ interface ChatContainerProps {
 const ChatContainer = ({ children }: ChatContainerProps): JSX.Element => (
   <div className="grid grid-cols-chat items-start">
     {children}
-    <div className="col-start-2 row-start-1 row-end-last self-stretch border-r-2" />
+    <div className="col-start-2 row-start-1 row-end-last self-stretch border-r-2 mt-16" />
   </div>
 );
 
