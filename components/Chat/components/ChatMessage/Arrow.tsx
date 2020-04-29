10708eff8697df5d2abf70814df18759baab027e
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-29 12:30:56 +0200

    Simplify rotation logic of arrows in chat messages

--- components/Chat/components/ChatMessage/Arrow.tsx
+++ components/Chat/components/ChatMessage/Arrow.tsx
@@ -6,11 +6,11 @@ interface ArrowProps {
 
 const Arrow = ({ position }: ArrowProps): JSX.Element => (
   <div
-    className={`w-0 h-0 mt-32 mb-6 border-t-48 border-b-48 ${
+    className={`hidden sm:block w-0 h-0 mt-32 mb-6 border-t-48 border-b-48 border-l-48 transform ${
       position === 'left'
-        ? 'border-orange-200 hidden sm:block border-r-48 md:border-r-0 md:border-l-48'
-        : 'border-blue-200 hidden sm:block border-l-48 md:border-l-0 md:border-r-48'
-    } `}
+        ? 'border-orange-200 rotate-180 md:rotate-0'
+        : 'border-blue-200 rotate-0 md:rotate-180'
+    }`}
     style={{
       borderTopColor: 'transparent',
       borderBottomColor: 'transparent',
