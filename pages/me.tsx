3102d2b27f9b26570f82c388f281d0c34a9dc385
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-29 09:24:08 +0200

    Add user page (route: /me)

---
+++ pages/me.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useMeUserInfoQuery } from '../graphql/generated/graphql';
+import UserInfo from '../components/UserInfo';
+import withApollo from '../utils/withApollo';
+
+const Me = (): JSX.Element | null => {
+  const { called, loading, data, error } = useMeUserInfoQuery();
+  const router = useRouter();
+
+  if (!called || loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error || !data) {
+    return (
+      <p>
+        Fehler
+        <br />
+        {JSON.stringify(error)}
+      </p>
+    );
+  }
+
+  if (!data.me) {
+    router.push(`/api/login?redirectTo=${router.asPath}`);
+    return null;
+  }
+
+  return <UserInfo user={data.me} />;
+};
+
+export default withApollo(Me);
