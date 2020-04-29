84302d7490ef5908e74e1634ecd79f7d6676ab12
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-29 14:05:29 +0200

    Move chat bubble design from chat message into separate component

--- components/Chat/components/ChatMessage/ChatMessage.tsx
+++ components/Chat/components/ChatMessage/ChatMessage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ChatMessageFragment } from '../../../../graphql/generated/graphql';
-import Arrow from './Arrow';
-import TextboxWithAuthorPicture from './TextboxWithAuthorPicture';
 import DateTime from './DateTime';
+import ChatBubble from './ChatBubble';
 
 interface ChatMessageProps {
   message: ChatMessageFragment | ChatMessageFragment[];
@@ -20,29 +19,16 @@ const ChatMessage = ({
   const firstMessage = messages[0];
 
   return (
-    <div
-      className={`${
-        position === 'left'
-          ? 'md:col-start-1 md:mb-24 sm:pr-12 md:pr-0 flex-row-reverse md:flex-row'
-          : 'md:col-start-3 md:mt-24 sm:pl-12 md:pl-0 flex-row md:flex-row-reverse'
-      } flex text-xl text-gray-800 font-text-message`}
-    >
-      <TextboxWithAuthorPicture
-        authorPicture={firstMessage.author.picture ?? ''}
-        authorId={firstMessage.author.id}
-        position={position}
-      >
-        {messages.map(message => (
-          <div key={message.id}>
-            <p>{message.text}</p>
-            <p>
-              <DateTime dateTime={message.createdAt} />
-            </p>
-          </div>
-        ))}
-      </TextboxWithAuthorPicture>
-      <Arrow position={position} />
-    </div>
+    <ChatBubble author={firstMessage.author} position={position}>
+      {messages.map(message => (
+        <div key={message.id}>
+          <p>{message.text}</p>
+          <p>
+            <DateTime dateTime={message.createdAt} />
+          </p>
+        </div>
+      ))}
+    </ChatBubble>
   );
 };
 
