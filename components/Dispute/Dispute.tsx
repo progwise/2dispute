af9c19fbd472836b377681893aeb89724ab8f57b
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-29 15:53:28 +0200

    Refetch messages after a reply on a dispute
    close #26

---
+++ components/Dispute/Dispute.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  useGetDisputeQuery,
+  useReplyOnDisputeMutation,
+} from '../../graphql/generated/graphql';
+import { ChatFormValues, DisputeChat } from '../Chat';
+import SubjectHeader from '../Subject/SubjectHeader';
+import Link from '../Link/Link';
+
+interface DisputeProps {
+  disputeId: string;
+}
+
+const Dispute = ({ disputeId }: DisputeProps): JSX.Element => {
+  const { called, data, loading, error, refetch } = useGetDisputeQuery({
+    variables: { disputeId },
+    pollInterval: 10 * 1000,
+  });
+
+  const [replyOnDispute] = useReplyOnDisputeMutation();
+
+  const handleNewMessage = async (values: ChatFormValues): Promise<void> => {
+    const { data, errors } = await replyOnDispute({
+      variables: { disputeId, message: values.message },
+    });
+
+    if (errors || data === undefined) throw new Error('submit failed');
+
+    // Normally the cache should update the list.
+    // But there is a bug: https://github.com/apollographql/react-apollo/issues/3816
+    await refetch();
+  };
+
+  if (!called || loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error || !data?.dispute) {
+    return (
+      <p>
+        Fehler
+        <br />
+        {JSON.stringify(error)}
+      </p>
+    );
+  }
+
+  const otherDisputesOfSubject = data.dispute.subject.disputes.filter(
+    dispute => dispute.id !== disputeId,
+  );
+
+  return (
+    <>
+      <SubjectHeader
+        subject={data.dispute.subject.subject}
+        tweetId={data.dispute.subject.tweetId ?? undefined}
+      />
+      <DisputeChat
+        dispute={data.dispute}
+        me={data.me}
+        onNewMessage={handleNewMessage}
+      />
+      <div>
+        Andere Dispute zu diesem Thema:
+        {otherDisputesOfSubject.length === 0 ? (
+          'Es existieren keine weiteren Dispute zu diesem Thema'
+        ) : (
+          <ul className="list-disc pl-8">
+            {otherDisputesOfSubject.map(dispute => (
+              <li key={dispute.id}>
+                <Link href="/dispute/[disputeId]" as={`/dispute/${dispute.id}`}>
+                  Disput zwischen {dispute.partnerA.name} und{' '}
+                  {dispute.partnerB.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Dispute;
--- pages/dispute/[disputeId].tsx
+++ pages/dispute/[disputeId].tsx
@@ -1,88 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
-import {
-  useGetDisputeLazyQuery,
-  useReplyOnDisputeMutation,
-} from '../../graphql/generated/graphql';
+import Dispute from '../../components/Dispute/Dispute';
 import withApollo from '../../utils/withApollo';
-import SubjectHeader from '../../components/Subject/SubjectHeader';
-import { DisputeChat, ChatFormValues } from '../../components/Chat';
-import Link from '../../components/Link/Link';
 
-const Dispute = (): JSX.Element => {
+const DisputePage = (): JSX.Element => {
   const router = useRouter();
   const disputeId = Array.isArray(router.query.disputeId)
     ? router.query.disputeId[0]
     : router.query.disputeId;
 
-  const [
-    loadDispute,
-    { called, loading, data, error },
-  ] = useGetDisputeLazyQuery({ pollInterval: 10 * 1000 });
-  const [replyOnDispute] = useReplyOnDisputeMutation();
+  if (disputeId === undefined) return <p>Loading...</p>;
 
-  const handleNewMessage = async (values: ChatFormValues): Promise<void> => {
-    const { data, errors } = await replyOnDispute({
-      variables: { disputeId, message: values.message },
-    });
-
-    if (errors || data === undefined) throw new Error('submit failed');
-  };
-
-  useEffect(() => {
-    if (disputeId !== undefined) {
-      loadDispute({ variables: { disputeId } });
-    }
-  }, [disputeId]);
-
-  if (!called || loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error || !data?.dispute) {
-    return (
-      <p>
-        Fehler
-        <br />
-        {JSON.stringify(error)}
-      </p>
-    );
-  }
-
-  const otherDisputesOfSubject = data.dispute.subject.disputes.filter(
-    dispute => dispute.id !== disputeId,
-  );
-
-  return (
-    <>
-      <SubjectHeader
-        subject={data.dispute.subject.subject}
-        tweetId={data.dispute.subject.tweetId ?? undefined}
-      />
-      <DisputeChat
-        dispute={data.dispute}
-        me={data.me}
-        onNewMessage={handleNewMessage}
-      />
-      <div>
-        Andere Dispute zu diesem Thema:
-        {otherDisputesOfSubject.length === 0 ? (
-          'Es existieren keine weiteren Dispute zu diesem Thema'
-        ) : (
-          <ul className="list-disc pl-8">
-            {otherDisputesOfSubject.map(dispute => (
-              <li key={dispute.id}>
-                <Link href="/dispute/[disputeId]" as={`/dispute/${dispute.id}`}>
-                  Disput zwischen {dispute.partnerA.name} und{' '}
-                  {dispute.partnerB.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </>
-  );
+  return <Dispute disputeId={disputeId} />;
 };
 
-export default withApollo(Dispute);
+export default withApollo(DisputePage);
