0ab8e55b8dba3c5c8aaca91353856081471d5409
Pascal Helbig <pascalhelbig@gmail.com>
2020-04-30 09:48:37 +0200

    Fix text overflow in chat bubbles
    close #31

--- components/Chat/components/ChatMessage/ChatBubble.tsx
+++ components/Chat/components/ChatMessage/ChatBubble.tsx
@@ -19,7 +19,7 @@ const ChatBubble = ({
       position === 'left'
         ? 'md:col-start-1 md:mb-24 sm:pr-12 md:pr-0 flex-row-reverse md:flex-row'
         : 'md:col-start-3 md:mt-24 sm:pl-12 md:pl-0 flex-row md:flex-row-reverse'
-    } flex text-xl text-gray-800 font-text-message`}
+    } flex text-xl text-gray-800 font-text-message break-words`}
   >
     <TextboxWithAuthorPicture
       authorPicture={author.picture ?? ''}
