b12a65221c76925a803d867933a75bd2eca6cf20
Pascal Helbig <pascalhelbig@gmail.com>
2020-05-05 09:21:49 +0200

    Improve form handling

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,40 +1,31 @@
 import React from 'react';
+import { useField, useFormikContext } from 'formik';
 import InputError from './InputError';
 
 interface InputProps {
   label: string;
-  name?: string;
+  name: string;
   placeholder: string;
-  value?: string | number;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
-  error?: string;
-  disabled?: boolean;
 }
 
-const Input = ({
-  label,
-  placeholder,
-  onChange,
-  onBlur,
-  name,
-  value,
-  error,
-  disabled = false,
-}: InputProps): JSX.Element => (
-  <label className="w-full flex flex-col text-center py-2">
-    <span className="text-blue-600">{label}</span>
-    <input
-      className="border-2 rounded text-center text-lg disabled:opacity-75"
-      name={name}
-      placeholder={placeholder}
-      onChange={onChange}
-      onBlur={onBlur}
-      value={value}
-      disabled={disabled}
-    />
-    {error && <InputError error={error} />}
-  </label>
-);
+const Input = ({ label, placeholder, name }: InputProps): JSX.Element => {
+  const [field, meta] = useField(name);
+  const { isSubmitting } = useFormikContext();
+
+  const error = meta.touched ? meta.error : undefined;
+
+  return (
+    <label className="w-full flex flex-col text-center py-2">
+      <span className="text-blue-600">{label}</span>
+      <input
+        className="border-2 rounded text-center text-lg disabled:opacity-75"
+        placeholder={placeholder}
+        disabled={isSubmitting}
+        {...field}
+      />
+      {error && <InputError error={error} />}
+    </label>
+  );
+};
 
 export default Input;
