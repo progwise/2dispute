7e3937552ba8d35ac9553bb60b4637d01a62a133
Pascal Helbig <pascalhelbig@gmail.com>
2020-05-05 15:47:16 +0200

    Add graphql field dispute to Message type

--- graphql/Message/resolvers.ts
+++ graphql/Message/resolvers.ts
@@ -4,6 +4,14 @@ const resolvers: MessageResolvers = {
   id: parent => parent._id.toString(),
   author: (parent, _args, context) =>
     context.dataloaders.userDataloader.load(parent.authorId),
+  dispute: async (parent, _args, context) => {
+    const data = await context.mongoose.models.Subject.aggregate()
+      .unwind('disputes')
+      .match({ 'disputes.messages._id': parent._id })
+      .exec();
+
+    return data[0].disputes || undefined;
+  },
 };
 
 export default resolvers;
