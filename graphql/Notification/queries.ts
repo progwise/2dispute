f6aa297d21e2641948beb738856f839770eae74e
Pascal Helbig <pascalhelbig@gmail.com>
2020-05-06 09:25:32 +0200

    Change notification query to notification connection

--- graphql/Notification/queries.ts
+++ graphql/Notification/queries.ts
@@ -1,18 +1,51 @@
 import { ApolloError } from 'apollo-server-micro';
 import { QueryResolvers } from '../generated/graphql';
+import DocumentConnectionResolver from '../helper/ConnectionResolver/DocumentConnectionResolver';
+import {
+  NewDisputeNotificationDocument,
+  NewMessageNotificationDocument,
+} from './NotificationSchema';
 
 const queries: QueryResolvers = {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore
-  meNotification: (_parent, _args, context) => {
+  allNotifications: async (_parent, args, context) => {
     if (!context.user) {
       throw new ApolloError('User not found');
     }
 
-    return context.mongoose.models.Notification.find()
+    const connectionResolver = new DocumentConnectionResolver<
+      NewDisputeNotificationDocument | NewMessageNotificationDocument
+    >(
+      { args, sortString: '-createdAt _id' },
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      context.mongoose.models.Notification,
+      { userId: context.user.id },
+    );
+
+    const totalCountRequest = context.mongoose.models.Notification.find()
       .where('userId', context.user.id)
-      .sort('-createdAt')
+      .count()
       .exec();
+
+    const totalCountUnreadRequest = context.mongoose.models.Notification.find()
+      .where('userId', context.user.id)
+      .where('read', false)
+      .count()
+      .exec();
+
+    const connectionRequest = connectionResolver.getConnection();
+
+    const [totalCount, totalCountUnread, connection] = await Promise.all([
+      totalCountRequest,
+      totalCountUnreadRequest,
+      connectionRequest,
+    ]);
+
+    return {
+      ...connection,
+      totalCount,
+      totalCountUnread,
+    };
   },
 };
 
