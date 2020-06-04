9769bac2aeb8f0d9b16f34c6cf8408b145f610d9
Pascal Helbig <pascalhelbig@gmail.com>
2020-06-04 09:25:50 +0200

    Remove more useless auth0 files

--- graphql/User/auth0ManagementClient.ts
+++
@@ -1,8 +0,0 @@
-import { ManagementClient } from 'auth0';
-
-export default new ManagementClient({
-  domain: process.env.AUTH0_DOMAIN ?? '',
-  clientId: process.env.AUTH0_MANAGEMENT_CLIENT_ID,
-  clientSecret: process.env.AUTH0_MANAGEMENT_CLIENT_SECRET,
-  scope: 'read:users',
-});
