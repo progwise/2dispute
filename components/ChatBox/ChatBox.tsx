6dba8149fe0eb2d8b9beee2ef96e14ebe9f86687
Pascal Helbig <pascalhelbig@gmail.com>
2020-07-08 10:53:02 +0200

    Add search box to chat list

--- components/ChatBox/ChatBox.tsx
+++ components/ChatBox/ChatBox.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dispute from '../Dispute/Dispute';
 import { useFullPage } from '../FullPage';
 import Link from '../Link/Link';
 import ChatList from './ChatList';
+import SearchBox from './SearchBox';
 
 interface ChatBoxProps {
   selectedDisputeId?: string;
 }
 
 const ChatBox = ({ selectedDisputeId }: ChatBoxProps): JSX.Element => {
+  const [search, setSearch] = useState('');
   useFullPage();
 
   const isDisputeSelected = selectedDisputeId !== undefined;
@@ -16,11 +18,14 @@ const ChatBox = ({ selectedDisputeId }: ChatBoxProps): JSX.Element => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-chatBox col-gap-8 h-full text-gray-900">
       <div
-        className={`bg-gray-100 overflow-y-auto ${
-          isDisputeSelected && 'hidden md:block'
+        className={`h-full min-h-full md:flex flex-col bg-gray-10 ${
+          isDisputeSelected && 'hidden'
         }`}
       >
-        <ChatList selectedDisputeId={selectedDisputeId} />
+        <SearchBox onChange={setSearch} />
+        <div className="overflow-y-auto">
+          <ChatList selectedDisputeId={selectedDisputeId} search={search} />
+        </div>
       </div>
       <div
         className={`h-full min-h-full flex flex-col ${
