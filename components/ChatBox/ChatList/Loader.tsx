fac88ec3819a3fed29fb4da94cf0dce836fdadef
Pascal Helbig <pascalhelbig@gmail.com>
2020-07-08 10:50:44 +0200

    Change loading text in ChatBoxList to a spinner

---
+++ components/ChatBox/ChatList/Loader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Loader = (): JSX.Element => (
+  <div className="border-2 border-gray-200 border-t-blue-600 h-20 w-20 rounded-full spin" />
+);
+
+export default Loader;
