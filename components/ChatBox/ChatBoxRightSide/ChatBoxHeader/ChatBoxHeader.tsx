d47b33fad75d5e261ec82ca126f13fd8c87c5e69
Pascal Helbig <pascalhelbig@gmail.com>
2020-08-12 16:20:04 +0200

    Add Roster to chatbox header

--- components/ChatBox/ChatBoxRightSide/ChatBoxHeader/ChatBoxHeader.tsx
+++ components/ChatBox/ChatBoxRightSide/ChatBoxHeader/ChatBoxHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import Link from '../../../Link/Link';
+import LogoSVG from '../../../Header/Logo/Logo.svg';
 
 interface ChatBoxHeaderProps {
   header: string;
@@ -14,14 +15,15 @@ const ChatBoxHeader = ({
   <div
     className={`row-start-1 col-start-1 md:col-start-2 md:col-span-2 border-b px-4 flex items-center py-2 ${
       displayOnSmallDevices ? '' : 'hidden'
-    } md:flex truncate`}
+    } md:flex truncate space-x-2`}
   >
-    <Link href="/" className="p-2 text-xl md:hidden">
+    <Link href="/" className="py-2 text-xl md:hidden">
       <FaArrowLeft />
     </Link>
-    <span className="py-2 truncate" title={header}>
+    <span className="py-2 truncate flex-grow" title={header}>
       {header}
     </span>
+    <img src={LogoSVG} className="h-8 w-8" />
   </div>
 );
 
