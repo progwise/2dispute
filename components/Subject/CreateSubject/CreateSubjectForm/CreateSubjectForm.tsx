70d9d3d6d9e35f194206570c62bff063034f0d02
Pascal Helbig <pascalhelbig@gmail.com>
2020-08-18 14:48:41 +0200

    Create new subject: Display tweets on button click

--- components/Subject/CreateSubject/CreateSubjectForm/CreateSubjectForm.tsx
+++ components/Subject/CreateSubject/CreateSubjectForm/CreateSubjectForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 import { Form, useFormikContext } from 'formik';
+import { FaCaretSquareDown } from 'react-icons/fa';
 import { Input, TextareaInput } from '../../../Input';
 import Button from '../../../Button/Button';
 import ChatContainer from '../../../Chat/components/ChatContainer';
@@ -12,13 +13,8 @@ import Loader from '../../../ChatBox/ChatList/Loader';
 import { getTweetId, FormValues } from '../CreateSubjectProvider';
 import SelectTweet from './SelectTweet';
 
-enum State {
-  Form,
-  SelectTweet,
-}
-
 const CreateSubjectForm = (): JSX.Element | null => {
-  const [state, setState] = useState<State>(State.Form);
+  const [displayTweetSelect, setDisplayTweetSelect] = useState(false);
   const { data, loading, error } = useMeQuery();
   const router = useRouter();
 
@@ -27,8 +23,11 @@ const CreateSubjectForm = (): JSX.Element | null => {
 
   const handleSelectTweet = (tweetLink: string): void => {
     formik.setFieldValue('tweetLink', tweetLink);
-    setState(State.Form);
+    setDisplayTweetSelect(false);
   };
+
+  const toggleDisplayTweets = (): void =>
+    setDisplayTweetSelect(oldState => !oldState);
 
   if (loading) {
     return <Loader />;
@@ -43,64 +42,54 @@ const CreateSubjectForm = (): JSX.Element | null => {
     return null;
   }
 
-  console.log(formik.values);
-
-  switch (state) {
-    case State.Form:
-      return (
-        <Form>
-          <div>
-            <Input
-              name="tweetLink"
-              label="Tweet:"
-              placeholder="https://twitter.com/..."
+  return (
+    <Form>
+      <div>
+        <div className="flex space-x-2 items-center text-blue-600">
+          <Input
+            className="flex-grow"
+            name="tweetLink"
+            label="Tweet:"
+            placeholder="https://twitter.com/..."
+          />
+          <FaCaretSquareDown
+            className="text-lg cursor-pointer"
+            onClick={(): void => toggleDisplayTweets()}
+          />
+        </div>
+        {displayTweetSelect ? (
+          <SelectTweet
+            onCancel={(): void => setDisplayTweetSelect(false)}
+            onSelect={handleSelectTweet}
+          />
+        ) : null}
+        {tweetId && (
+          <div className="px-4 mx-auto">
+            <TwitterTweetEmbed
+              tweetId={tweetId}
+              key={tweetId}
+              placeholder="Lade Tweet..."
+              className="mx-auto"
+              options={{
+                lang: 'de',
+                align: 'center',
+              }}
             />
-            <p className="text-center">
-              <a
-                className="cursor-pointer text-blue-600 underline"
-                onClick={(): void => setState(State.SelectTweet)}
-              >
-                Tweet aus deiner Timeline auswählen
-              </a>
-            </p>
-            {tweetId && (
-              <div className="px-4 mx-auto">
-                <TwitterTweetEmbed
-                  tweetId={tweetId}
-                  key={tweetId}
-                  placeholder="Lade Tweet..."
-                  className="mx-auto"
-                  options={{
-                    lang: 'de',
-                    align: 'center',
-                  }}
-                />
-              </div>
-            )}
-            <ChatContainer>
-              <ChatBubble position="left" author={data.me}>
-                <TextareaInput
-                  name="firstMessage"
-                  placeholder="Deine Position *"
-                />
-              </ChatBubble>
-            </ChatContainer>
           </div>
-          <ChatItemFullWidth className="py-4 flex justify-center">
-            <Button type="submit" disabled={formik.isSubmitting}>
-              Thema veröffentlichen
-            </Button>
-          </ChatItemFullWidth>
-        </Form>
-      );
-    case State.SelectTweet:
-      return (
-        <SelectTweet
-          onCancel={(): void => setState(State.Form)}
-          onSelect={handleSelectTweet}
-        />
-      );
-  }
+        )}
+        <ChatContainer>
+          <ChatBubble position="left" author={data.me}>
+            <TextareaInput name="firstMessage" placeholder="Deine Position *" />
+          </ChatBubble>
+        </ChatContainer>
+      </div>
+      <ChatItemFullWidth className="py-4 flex justify-center">
+        <Button type="submit" disabled={formik.isSubmitting}>
+          Thema veröffentlichen
+        </Button>
+      </ChatItemFullWidth>
+    </Form>
+  );
 };
 
 export default CreateSubjectForm;
