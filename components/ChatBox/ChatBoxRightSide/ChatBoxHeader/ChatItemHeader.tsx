fa365ffeb29fcb6c20999d27cf84c104e7736372
Pascal Helbig <pascalhelbig@gmail.com>
2020-08-25 16:44:41 +0200

    Load disputes on subject/dispute pages on interval
    closes #29

--- components/ChatBox/ChatBoxRightSide/ChatBoxHeader/ChatItemHeader.tsx
+++ components/ChatBox/ChatBoxRightSide/ChatBoxHeader/ChatItemHeader.tsx
@@ -14,6 +14,7 @@ const ChatItemHeader = ({
 }: ChatItemHeaderProps): JSX.Element => {
   const { data } = useChatItemHeaderQuery({
     variables: { chatItemId },
+    pollInterval: 60 * 1000,
   });
 
   const chatItem = data?.chatItem;
