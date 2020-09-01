82c07994da991359d0f18198f234ea76264efc33
Pascal Helbig <pascalhelbig@gmail.com>
2020-09-01 12:23:08 +0200

    When a user views own user route redirect to /me

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -6,15 +6,21 @@ import {
   ChatBoxRightSideContent,
   ChatBoxHeader,
 } from '../../components/ChatBox/ChatBoxRightSide';
+import useUser from '../../utils/react-hooks/useUser';
 
 const UserPage = (): JSX.Element => {
   const router = useRouter();
+  const user = useUser();
   const userId: string | undefined = Array.isArray(router.query.userId)
     ? router.query.userId[0]
     : router.query.userId;
 
   if (userId === undefined) {
     return <ChatBoxRightSideContent>Loading</ChatBoxRightSideContent>;
+  }
+
+  if (userId === user?.twitterId) {
+    router.replace('/me');
   }
 
   return (
