5dedcae82e02586feceb1d6144cc1b4c7a784306
Pascal Helbig <pascalhelbig@gmail.com>
2020-09-01 10:21:18 +0200

    Remove separate query for /me route

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { useMeUserInfoQuery } from '../graphql/generated/frontend';
-import UserInfo from '../components/UserInfo';
 import withApollo from '../utils/withApollo';
 import { ChatBoxRightSideContent } from '../components/ChatBox/ChatBoxRightSide';
+import useUser from '../utils/react-hooks/useUser';
+import { UserInfoById } from '../components/UserInfo';
 
 const Me = (): JSX.Element | null => {
-  const { called, loading, data, error } = useMeUserInfoQuery();
   const router = useRouter();
+  const user = useUser();
 
-  if (!called || loading) {
-    return <ChatBoxRightSideContent>Loading...</ChatBoxRightSideContent>;
-  }
-
-  if (error || !data) {
-    return (
-      <ChatBoxRightSideContent>
-        Fehler
-        <br />
-        {JSON.stringify(error)}
-      </ChatBoxRightSideContent>
-    );
-  }
-
-  if (!data.me) {
-    router.push(`/api/auth/twitter?redirectTo=${router.asPath}`);
+  if (!user) {
+    if (typeof window !== 'undefined') {
+      router.push(`/api/auth/twitter?redirectTo=${router.asPath}`);
+    }
     return null;
   }
 
   return (
     <ChatBoxRightSideContent>
-      <UserInfo user={data.me} />
+      <UserInfoById userId={user.twitterId} />
     </ChatBoxRightSideContent>
   );
 };
